b1fa1196a7fe33aeb90e5cd99cc6fc10f534bbaa
Nagesh Rathod <111345802+nageshredhat@users.noreply.github.com>
2024-01-29 15:00:55 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('hello-world-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Red Hat Developers are awesome');
   });
 });
